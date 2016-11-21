9933cf99eb1a66b2dfbcc6b806d2d56ba7299510
ThaJay <ThaJay@users.noreply.github.com>
2016-11-21 13:49:12 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,14 @@
+mv ~/.bash_aliases ~/.bash_aliases.old
+mv ~/.bash_profile ~/.bash_profile.old
+mv ~/.bashrc ~/.bashrc.old
+mv ~/.config/ ~/.config/.old
+mv ~/.gitconfig ~/.gitconfig.old
+mv ~/.gitignore ~/.gitignore.old
+mv ~/.profile ~/.profile.old
+mv ~/.tmux.conf ~/.tmux.conf.old
+
+mkdir -p ~/.config/sublime-text-3/Packages/User
+
 ln -s $PWD/.config/sublime-text-3/Packages/User/ ~/.config/sublime-text-3/Packages/User
 ln -s $PWD/.bashrc ~/.bashrc
 ln -s $PWD/.bash_aliases ~/.bash_aliases
