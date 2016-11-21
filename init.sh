cc5fd1de6064499180c411f5b848ac25216c6078
ThaJay <ThaJay@users.noreply.github.com>
2016-11-21 13:52:58 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,13 +7,31 @@ mv ~/.gitignore ~/.gitignore.old
 mv ~/.profile ~/.profile.old
 mv ~/.tmux.conf ~/.tmux.conf.old
 
+print "renamed already present files with '.old' postfix"
+
 mkdir -p ~/.config/sublime-text-3/Packages/User
+print "Sublime Text 3 User folder recreated"
 
 ln -s $PWD/.config/sublime-text-3/Packages/User/ ~/.config/sublime-text-3/Packages/User
+print "Symlink created to Sublime Text 3 User folder"
+
 ln -s $PWD/.bashrc ~/.bashrc
+print "Symlink created to .bashrc"
+
 ln -s $PWD/.bash_aliases ~/.bash_aliases
+print "Symlink created to .bash_aliases"
+
 ln -s $PWD/.bash_profile ~/.bash_profile
+print "Symlink created to .bash_profile"
+
 ln -s $PWD/.gitconfig ~/.gitconfig
+print "Symlink created to .gitconfig"
+
 ln -s $PWD/.gitignore ~/.gitignore
+print "Symlink created to .gitignore"
+
 ln -s $PWD/.profile ~/.profile
+print "Symlink created to .profile"
+
 ln -s $PWD/.tmux.conf ~/.tmux.conf
+print "Symlink created to .tmux.conf"
