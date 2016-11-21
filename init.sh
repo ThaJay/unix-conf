e2dbd6e81301269f41eee919103c2d47090aa7d1
ThaJay <ThaJay@users.noreply.github.com>
2016-11-21 13:54:05 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,31 +7,31 @@ mv ~/.gitignore ~/.gitignore.old
 mv ~/.profile ~/.profile.old
 mv ~/.tmux.conf ~/.tmux.conf.old
 
-print "renamed already present files with '.old' postfix"
+printf "renamed already present files with '.old' postfix"
 
 mkdir -p ~/.config/sublime-text-3/Packages/User
-print "Sublime Text 3 User folder recreated"
+printf "Sublime Text 3 User folder recreated"
 
 ln -s $PWD/.config/sublime-text-3/Packages/User/ ~/.config/sublime-text-3/Packages/User
-print "Symlink created to Sublime Text 3 User folder"
+printf "Symlink created to Sublime Text 3 User folder"
 
 ln -s $PWD/.bashrc ~/.bashrc
-print "Symlink created to .bashrc"
+printf "Symlink created to .bashrc"
 
 ln -s $PWD/.bash_aliases ~/.bash_aliases
-print "Symlink created to .bash_aliases"
+printf "Symlink created to .bash_aliases"
 
 ln -s $PWD/.bash_profile ~/.bash_profile
-print "Symlink created to .bash_profile"
+printf "Symlink created to .bash_profile"
 
 ln -s $PWD/.gitconfig ~/.gitconfig
-print "Symlink created to .gitconfig"
+printf "Symlink created to .gitconfig"
 
 ln -s $PWD/.gitignore ~/.gitignore
-print "Symlink created to .gitignore"
+printf "Symlink created to .gitignore"
 
 ln -s $PWD/.profile ~/.profile
-print "Symlink created to .profile"
+printf "Symlink created to .profile"
 
 ln -s $PWD/.tmux.conf ~/.tmux.conf
-print "Symlink created to .tmux.conf"
+printf "Symlink created to .tmux.conf"
