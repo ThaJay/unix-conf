55f2b2c2a1e02b99f9c2052c296d13682bd05ace
Webhooks <webhooks@codeerbedrijf.nl>
2023-06-22 10:39:25 +0200

    change github email and name to webhooks remove unneeded files for server

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,9 +0,0 @@
-# Set ~ to equal $home (C:\Users\DJ)
-set-variable ~ $home
-
-# set custom color scheme
-# concfg import -n mountain
-# concfg import -n C:\Users\DJ\Documents\WindowsPowerShell\concfg_profile.json
-
-# start in root
-Set-Location C:\dev
